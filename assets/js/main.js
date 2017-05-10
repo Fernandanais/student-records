ac357c920f2c6ff85c0b77457e4a2f8eca4c2d7c
Diana Villasmil <diacavipi@gmail.com>
2017-05-10 05:48:57 -0300

    refactorizacion, validadores noVacio y porcentaje, formato titulo, formato numeros, alert con errores de validacion, cambio variable Estudiante a estudiante, cambio de active a Active

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,71 @@
 var studentsList = [];
 
-function campoNoVacio(mensaje){
-    var salida = "";
-    do {
-      salida = prompt(mensaje);
-    }
+function toTitleCase(str){
+    return str.split(" ").map((str)=>{return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();}).join(" ");
+}
 
-    while (salida == "");
+function validarPorcentaje(str){
+    num = parseFloat(str);
+    return (!isNaN(num) && num >= 0 && num <= 100);
+}
+
+function validarNoVacio(str){
+    return (str != '');
+}
+
+function leerCampo(mensaje,validaciones = []){
+    var salida = "";    
+    do {
+      var error = false;
+      salida = prompt(mensaje);
+      validaciones.forEach((val)=>{
+        switch(val){
+            case 'noVacio': 
+                if (!validarNoVacio(salida)){
+                    alert("El campo no debe estar vacío");
+                    error = true;
+                }
+                break;
+            case 'porcentaje': 
+                if (!validarPorcentaje(salida)){
+                    alert("El campo debe contener solo valores numéricos entre 0 y 100");
+                    error = true;
+                } 
+                break;
+            default: break;
+        }
+      });
+    } while (error);
+
     return salida;
 }
 
 function botonAddStudent(){
-    var nombre = campoNoVacio("¿Cual es tu nombre?");
-    var porcentajeTecnico = campoNoVacio("¿Cual es tu porcentaje tecnico?");
-    var porcentajeEmocional = campoNoVacio("¿Cual es tu porcentaje socio-emocional?");
+    var nombre = toTitleCase(leerCampo("¿Cual es tu nombre?",['noVacio']));
+    var porcentajeTecnico = parseFloat(leerCampo("¿Cual es tu porcentaje tecnico? (0-100)",['noVacio','porcentaje'])).toFixed(2);
+    var porcentajeEmocional = parseFloat(leerCampo("¿Cual es tu porcentaje socio-emocional? (0-100)",['noVacio','porcentaje'])).toFixed(2);
 
-    var Estudiante = new Student(nombre,porcentajeTecnico,porcentajeEmocional);
+    var estudiante = new Student(nombre,porcentajeTecnico,porcentajeEmocional);
 
     document.getElementById("students").innerHTML = "";
-    printOneStudent(Estudiante);
+    printOneStudent(estudiante);
 
-    studentsList.push(Estudiante);
+    studentsList.push(estudiante);
 }
 
-function printOneStudent(Estudiante){
+function printOneStudent(estudiante){
     var student = document.getElementById("students");
-    student.innerHTML += "<div><p><strong>Nombre:</strong> " + Estudiante.nombre + "</p>" +
-        "<p><strong>Porcentaje Técnico:</strong> " + Estudiante.porcentajeTecnico + "</p>" +
-        "<p><strong>Porcentaje HSE:</strong> " + Estudiante.porcentajeEmocional + "</p>" +
-        "<p><strong>Estado:</strong> " + Estudiante.status + "</p></div>";
+    student.innerHTML += "<div><p><strong>Nombre:</strong> " + estudiante.nombre + "</p>" +
+        "<p><strong>Porcentaje Técnico:</strong> " + estudiante.porcentajeTecnico + "</p>" +
+        "<p><strong>Porcentaje HSE:</strong> " + estudiante.porcentajeEmocional + "</p>" +
+        "<p><strong>Estado:</strong> " + estudiante.status + "</p></div>";
 }
 
 function botonPrintAll(){
     var students = document.getElementById("students");
     students.innerHTML = "";
-    studentsList.forEach(function(Estudiante){
-        printOneStudent(Estudiante);
+    studentsList.forEach(function(estudiante){
+        printOneStudent(estudiante);
     });
 }
 
@@ -43,5 +73,5 @@ function Student(nombre,porcentajeTecnico,porcentajeEmocional){
     this.nombre = nombre;
     this.porcentajeTecnico = porcentajeTecnico;
     this.porcentajeEmocional = porcentajeEmocional;
-    this.status = "active";
+    this.status = "Active";
 }
